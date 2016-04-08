00bc5548a78134fa0c7aaf0ce8d6797723e10f56
makevertti <makevertti@users.noreply.github.com>
2016-04-08 19:19:28 +0300

    Abstract Citation has all needed fields so adding ArticleCitation works

--- src/main/java/com/ohtukki/citations/models/ArticleCitation.java
+++ src/main/java/com/ohtukki/citations/models/ArticleCitation.java
@@ -1,85 +1,13 @@
 package com.ohtukki.citations.models;
 
 public class ArticleCitation extends Citation {
-    // Required fields
-    private String journal;
-    private String year;
-
-    // Optional fields
-    private String volume;
-    private String number;
-    private String pages;
-    private String month;
-    private String note;
-    private String key;
-
     // temp. test to see that the citation fields get filled
     @Override
     public String toString(){
-        return "id: " + this.getId() + " author: " + this.getAuthor() + " name: " + this.getName();
+        return "id: " + this.getId() + " author: " + this.getAuthor() + " name: " + this.getName() +
+                " journal: " + this.getJournal() + " year: " + this.getYear() + " volume: " + this.getVolume() +
+                " number: " + this.getNumber() + " pages: " + this.getPages() + " month: " + this.getMonth() +
+                " note: " + this.getMonth() + " key: " + this.getKey();
     }
 
-    public String getJournal() {
-        return journal;
-    }
-
-    public void setJournal(String journal) {
-        this.journal = journal;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getVolume() {
-        return volume;
-    }
-
-    public void setVolume(String volume) {
-        this.volume = volume;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getPages() {
-        return pages;
-    }
-
-    public void setPages(String pages) {
-        this.pages = pages;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
 }
--- src/main/java/com/ohtukki/citations/models/Citation.java
+++ src/main/java/com/ohtukki/citations/models/Citation.java
@@ -7,6 +7,14 @@ public abstract class Citation {
     private String id;
     private String author;
     private String name;
+    private String journal;
+    private String year;
+    private String volume;
+    private String number;
+    private String pages;
+    private String month;
+    private String note;
+    private String key;
     
     public void setId(String id) {
         this.id = id;
@@ -25,5 +33,69 @@ public abstract class Citation {
     }
     public String getName() {
         return this.name;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 }

6725283a4100daca01bf04326b54d45edab8aeed
Kristian Lauttamus <kristian.lauttamus@gmail.com>
2016-04-18 16:26:26 +0300

    Removed CitationList

--- src/main/java/com/ohtukki/citations/data/DatabaseJsonDao.java
+++ src/main/java/com/ohtukki/citations/data/DatabaseJsonDao.java
@@ -2,7 +2,6 @@ package com.ohtukki.citations.data;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,8 +11,8 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.ohtukki.citations.models.Citation;
-import com.ohtukki.citations.models.CitationList;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,8 +45,7 @@ public class DatabaseJsonDao implements Database {
         } catch (IOException ex) {
             Logger.getLogger(DatabaseJsonDao.class.getName()).log(Level.SEVERE, null, ex);
         }
-        CitationList list = gson.fromJson(data, CitationList.class);
-        this.citations = list.getReferences();
+        this.citations = gson.fromJson(data, new TypeToken<ArrayList<Citation>>(){}.getType());
         
         return citations;
     }
@@ -89,8 +87,7 @@ public class DatabaseJsonDao implements Database {
     public void saveAll() {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        CitationList list = new CitationList(this.citations);
-        String json = gson.toJson(list);
+        String json = gson.toJson(this.citations);
         try {
             FileUtils.writeStringToFile(new File(filename), json);
         } catch (IOException ex) {
@@ -131,8 +128,7 @@ public class DatabaseJsonDao implements Database {
         
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        CitationList list = new CitationList(this.citations);
-        String json = gson.toJson(list);
+        String json = gson.toJson(this.citations);
         try {
             FileUtils.writeStringToFile(new File(filename), json);
         } catch (IOException ex) {

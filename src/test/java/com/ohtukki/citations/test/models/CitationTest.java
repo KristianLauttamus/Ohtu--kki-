a5dde2aca0f90500cbb3e89d13dd851d0c1aea7a
Kristian Lauttamus <kristian.lauttamus@gmail.com>
2016-04-20 15:38:56 +0300

    Some Citation tests

--- src/test/java/com/ohtukki/citations/test/models/CitationTest.java
+++ src/test/java/com/ohtukki/citations/test/models/CitationTest.java
@@ -5,9 +5,11 @@ import com.ohtukki.citations.models.*;
 import static org.junit.Assert.*;
 
 public class CitationTest {
+    public ArticleCitation articleCitation;
+    
     @Test
     public void unvalidFieldsWontGetThrough(){
-        ArticleCitation articleCitation = new ArticleCitation();
+        articleCitation = new ArticleCitation();
         articleCitation.requiredFields = new String[]{"id", "title"};
         
         articleCitation.setId("Not empty");
@@ -17,7 +19,7 @@ public class CitationTest {
     
     @Test
     public void validFieldsGetValidated(){
-        ArticleCitation articleCitation = new ArticleCitation();
+        articleCitation = new ArticleCitation();
         articleCitation.requiredFields = new String[]{"id", "title"};
         
         articleCitation.setId("Not empty");
@@ -52,5 +54,69 @@ public class CitationTest {
         assertTrue(new ProceedingsCitation().getRequiredFields()[0].equals("id"));
         assertTrue(new TechReportCitation().getRequiredFields()[0].equals("id"));
         assertTrue(new UnpublishedCitation().getRequiredFields()[0].equals("id"));
+    }
+    
+    @Test
+    public void allCitationTypesHaveCorrectCitationType(){
+        assertTrue(new ArticleCitation().getCitationType().equals("article"));
+        assertTrue(new BookCitation().getCitationType().equals("book"));
+        assertTrue(new BookletCitation().getCitationType().equals("booklet"));
+        assertTrue(new ConferenceCitation().getCitationType().equals("conference"));
+        assertTrue(new InbookCitation().getCitationType().equals("inbook"));
+        assertTrue(new IncollectionCitation().getCitationType().equals("incollection"));
+        assertTrue(new InproceedingsCitation().getCitationType().equals("inproceedings"));
+        assertTrue(new ManualCitation().getCitationType().equals("manual"));
+        assertTrue(new MastersthesisCitation().getCitationType().equals("mastersthesis"));
+        assertTrue(new MiscCitation().getCitationType().equals("misc"));
+        assertTrue(new PHDThesisCitation().getCitationType().equals("phdthesis"));
+        assertTrue(new ProceedingsCitation().getCitationType().equals("proceedings"));
+        assertTrue(new TechReportCitation().getCitationType().equals("techreport"));
+        assertTrue(new UnpublishedCitation().getCitationType().equals("unpublished"));
+    }
+    
+    @Test
+    public void createBibtexEntryWorksWithNullValues() {
+        articleCitation = new ArticleCitation();
+        assertEquals("@ARTICLE{\n}\n\n", articleCitation.createBibtexEntry());
+    }
+
+    @Test
+    public void createBibtexEntryWorksWithEmptyStrings() {
+        articleCitation = createArticleCitationWithEmptyStrings();
+        assertEquals("@ARTICLE{\n}\n\n", articleCitation.createBibtexEntry());
+    }
+
+    @Test
+    public void createBibtexEntryWorksWithSomeStrings() {
+        articleCitation = new ArticleCitation();
+        articleCitation.setAuthor("Pekka K. Kirjailija");
+        articleCitation.setId("PK95");
+        assertEquals("@ARTICLE{PK95,\nauthor = {Pekka K. Kirjailija},\n}\n\n",
+                articleCitation.createBibtexEntry());
+    }
+    
+    @Test
+    public void createBibtexEntryWorksWithUmlauts() {
+        articleCitation = new ArticleCitation();
+        articleCitation.setAuthor("Pekka Ö. Mäki");
+        articleCitation.setId("PK95");
+        assertEquals("@ARTICLE{PK95,\nauthor = {Pekka \\\"{O}. M\\\"{a}ki},\n}\n\n",
+                articleCitation.createBibtexEntry());
+    }
+    
+    private ArticleCitation createArticleCitationWithEmptyStrings() {
+        ArticleCitation temp = new ArticleCitation();
+        temp.setId("");
+        temp.setAuthor("");
+        temp.setTitle("");
+        temp.setJournal("");
+        temp.setYear("");
+        temp.setVolume("");
+        temp.setNumber("");
+        temp.setPages("");
+        temp.setMonth("");
+        temp.setNote("");
+        temp.setKey("");
+        return temp;
     }
 }
